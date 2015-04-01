51f2f673a85af8642130575a07586236cdb84744
Ashok Modi <btmash@gmail.com>
2015-04-01 11:05:02 -0700

    Add right url, little bit of css to start.

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
-  var xhr = $.get("http://api.giphy.com/v1/stickers/random?api_key=dc6zaTOxFJmzC&tag=backdrop");
-  xhr.done(function(data) { console.log("success got data", data); });
-}
+  var xhr = $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=wrestling+backdrop");
+  xhr.done(function(giphy) {
+    image_tag = '<img src="' + giphy.data.image_url + '" width="100%" alt="You have been backdropped!"/>';
+    $('.content').append(image_tag);
+    console.log("success got data", giphy.data);
+  });
+});
