98fae5f277d855442989523d0d774003c108d9d2
Ashok Modi <btmash@gmail.com>
2015-04-01 13:49:24 -0700

    Remove image tag and just apply to div.

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var xhr = $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=wrestling");
   xhr.done(function(giphy) {
-    image_tag = '<img src="' + giphy.data.image_url + '" width="100%" alt="You have been backdropped!"/>';
-    $('.content').append(image_tag);
+    image_background_css = 'url("' + giphy.data.image_url + '") no-repeat fixed center center / cover rgba(0, 0, 0, 0)';
+    $('.content').css('background', image_background_css);
   });
 });
