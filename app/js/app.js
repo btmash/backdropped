d9ea127e9c9b2c21fe551be07fef25e9234c7b85
Ashok Modi <btmash@gmail.com>
2015-04-01 10:36:03 -0700

    Get backdrop images.

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-  // Do something
+  var xhr = $.get("http://api.giphy.com/v1/stickers/random?api_key=dc6zaTOxFJmzC&tag=backdrop");
+  xhr.done(function(data) { console.log("success got data", data); });
 }
