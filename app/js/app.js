abfb8147dfc874275fae509880565a179880a9c6
Ashok Modi <btmash@gmail.com>
2015-04-01 14:28:01 -0700

    Semi-mimic the tv functionality.

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-  var xhr = $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=wrestling");
-  xhr.done(function(giphy) {
-    image_background_css = 'url("' + giphy.data.image_url + '") no-repeat fixed center center / cover rgba(0, 0, 0, 0)';
-    $('.content').css('background', image_background_css);
-  });
+  function replace_gif() {
+    var xhr = $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=wrestling");
+    xhr.done(function(giphy) {
+      image_background_css = 'url("' + giphy.data.image_url + '") no-repeat fixed center center / cover rgba(0, 0, 0, 0)';
+      $('.content').css('background', image_background_css);
+    });
+    setTimeout(function() { replace_gif(); }, 5000);
+  }
+  setTimeout(function() { replace_gif(); }, 1000);
 });
