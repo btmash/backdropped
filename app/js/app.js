27be84735d9c640ae9498659d96690d3ab416141
Ashok Modi <btmash@gmail.com>
2015-04-01 13:35:46 -0700

    Only search wrestling for more options. And remove console log.

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
-  var xhr = $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=wrestling+backdrop");
+  var xhr = $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=wrestling");
   xhr.done(function(giphy) {
     image_tag = '<img src="' + giphy.data.image_url + '" width="100%" alt="You have been backdropped!"/>';
     $('.content').append(image_tag);
-    console.log("success got data", giphy.data);
   });
 });
