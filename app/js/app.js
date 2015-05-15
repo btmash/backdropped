818e198e0c25299786d7b9f5b32b1d7445874873
Ashok Modi <btmash@gmail.com>
2015-05-15 00:53:40 -0700

    Show wrestling woo or backdrops.

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   function replace_gif() {
-    var xhr = $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=wrestling");
+    var tags = ['wrestling+woo', 'wrestling+backdrop'];
+    var xhr = $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + tags[Math.floor(Math.random()*tags.length)]);
     xhr.done(function(giphy) {
       image_background_css = 'url("' + giphy.data.image_url + '") no-repeat fixed center center / cover rgba(0, 0, 0, 0)';
       $('.content').css('background', image_background_css);
     });
-//    setTimeout(function() { play_scream(); }, 2000);
     setTimeout(function() { transition(); }, 10000);
   }
   function transition() {
