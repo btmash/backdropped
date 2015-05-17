80b2ee657d34979e117a6f4e76521e1817978c94
Ashok Modi <btmash@gmail.com>
2015-05-16 21:10:08 -0700

    Update rotate between woos and backdrops.

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
+  var position = 0;
   function replace_gif() {
+    position = (position + 1) % 2;
     var tags = ['ric+flair+woo+excited+vintage', 'wrestling+backdrop'];
-    var xhr = $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + tags[Math.floor(Math.random()*tags.length)]);
+    var xhr = $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + tags[position]);
     xhr.done(function(giphy) {
       image_background_css = 'url("' + giphy.data.image_url + '") no-repeat fixed center center / cover rgba(0, 0, 0, 0)';
       $('.content').css('background', image_background_css);
