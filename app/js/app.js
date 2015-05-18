11c38435afbecbc8651e5d656cd999a03f9baf40
Ashok Modi <btmash@gmail.com>
2015-05-18 15:39:14 -0700

    API now points to giphy.

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   function replace_gif() {
     position = (position + 1) % 2;
     var tags = ['ric+flair+woo+excited+vintage', 'wrestling+backdrop'];
-    var xhr = $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + tags[position]);
+    var xhr = $.get("http://tv.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + tags[position]);
     xhr.done(function(giphy) {
       image_background_css = 'url("' + giphy.data.image_url + '") no-repeat fixed center center / cover rgba(0, 0, 0, 0)';
       $('.content').css('background', image_background_css);
