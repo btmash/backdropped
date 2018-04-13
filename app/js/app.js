52e646e1eea6bacb08fd8bdc3b5d15504e2b7982
Ashok Modi <btmash@gmail.com>
2018-04-13 10:33:24 -0700

    Use https and try other things

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   var position = 0;
   function replace_gif() {
-    position = (position + 1) % 2;
-    var tags = ['ric+flair+woo+excited+vintage', 'wrestling+backdrop'];
-    var xhr = $.get("http://tv.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + tags[position]);
+    position = (position + 1) % 3;
+    var tags = ['ric-flair', 'hulk-hogan', 'randy-savage'];
+    var xhr = $.get("https://tv.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + tags[position]);
     xhr.done(function(giphy) {
       image_background_css = 'url("' + giphy.data.image_url + '") no-repeat fixed center center / cover rgba(0, 0, 0, 0)';
       $('.content').css('background', image_background_css);
